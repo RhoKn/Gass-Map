ddf2c880f292f4a05ebe9e936f4aef6495ab9260
Andree Amaro <andree0796@gmail.com>
2019-05-13 15:03:20 -0600

    Se ajustan unit testing para poder correr de forma independiente

--- src/tests/unittesting/unit.tests.js
+++ src/tests/unittesting/unit.tests.js
@@ -7,21 +7,21 @@ let r = Math.random().toString(36).substring(7);
 let emailR = r+'@gmail.com';
 
 describe('Functionality Step by Step', function(){
-    it("Sign up a user", function(done){
+    it("SignUP", function(done){
         request.post(uri+'users/register',{form:{nick_name: r, password: r,user_type:'true',email:emailR, first_name:r,last_name:r}}, function(error, response, body){
             expect(response.statusCode).to.equal(201);
             expect(response.statusMessage).to.equal('Created');
             done();
         });
     });
-    it("Login a user",function(done) {
+    it("Login",function(done) {
         request.post(uri+'users/login',{form:{nick_name: r, password:r}},function(error,response,body){
             expect(response.statusCode).to.equal(200);
             expect(response.statusMessage).to.equal('OK');
             done();
         });
     });
-    it('Get Gas Stations', function(done) {
+    it('List', function(done) {
         request(uri+'gasStations/all' , function(error, response, body) {
             expect(response.statusCode).to.equal(200);
             expect(response.statusMessage).to.equal('OK');
