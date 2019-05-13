46343e544e5f931a821bfe1f0619d8e5b4f7aa4d
Andree Amaro <andree0796@gmail.com>
2019-05-13 14:59:25 -0600

    Se agregan unit testings

--- src/tests/unittesting/unit.tests.js
+++ src/tests/unittesting/unit.tests.js
@@ -1,16 +1,28 @@
 const expect  = require('chai').expect;
 let request = require('request');
 
+const uri = 'http://localhost:3000/';
+
+let r = Math.random().toString(36).substring(7);
+let emailR = r+'@gmail.com';
+
 describe('Functionality Step by Step', function(){
+    it("Sign up a user", function(done){
+        request.post(uri+'users/register',{form:{nick_name: r, password: r,user_type:'true',email:emailR, first_name:r,last_name:r}}, function(error, response, body){
+            expect(response.statusCode).to.equal(201);
+            expect(response.statusMessage).to.equal('Created');
+            done();
+        });
+    });
     it("Login a user",function(done) {
-        request.post('http://localhost:3000/users/login',{form:{nick_name: 'admin', password:1}},function(error,response,body){
+        request.post(uri+'users/login',{form:{nick_name: r, password:r}},function(error,response,body){
             expect(response.statusCode).to.equal(200);
             expect(response.statusMessage).to.equal('OK');
             done();
         });
     });
-    it('Get our data', function(done) {
-        request('http://localhost:3000/gasStations/all' , function(error, response, body) {
+    it('Get Gas Stations', function(done) {
+        request(uri+'gasStations/all' , function(error, response, body) {
             expect(response.statusCode).to.equal(200);
             expect(response.statusMessage).to.equal('OK');
             done();
